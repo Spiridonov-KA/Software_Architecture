49aded0c832f57901ea5a018ce0e2f6308a4cfd9
Kirill Spriridonov <vo-ro@list.ru>
2024-05-26 21:44:48 +0300

    add 6th lab

---
+++ lab_06/database/user.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Poco/JSON/Object.h>
+
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace database {
+class User {
+private:
+    long _id;
+    std::string _first_name;
+    std::string _last_name;
+    std::string _email;
+    std::string _phone;
+    std::string _login;
+    std::string _password;
+    std::string _address;
+
+public:
+    static User fromJSON(const std::string& str);
+
+    long get_id() const { return _id; };
+    const std::string& get_first_name() const { return _first_name; };
+    const std::string& get_last_name() const { return _last_name; };
+    const std::string& get_email() const { return _email; };
+    const std::string& get_phone() const { return _phone; };
+    const std::string& get_login() const { return _login; };
+    const std::string& get_password() const { return _password; };
+    const std::string& get_address() const { return _address; };
+
+    long& id() { return _id; };
+    std::string& first_name() { return _first_name; };
+    std::string& last_name() { return _last_name; };
+    std::string& email() { return _email; };
+    std::string& phone() { return _phone; };
+    std::string& login() { return _login; };
+    std::string& password() { return _password; };
+    std::string& address() { return _address; };
+
+    bool check_name(std::string& reason);
+    bool check_email(std::string& reason);
+    void hash_password();
+
+    static std::optional<User> get_by_id(long id);
+    static std::vector<User> search_by_name(std::string first_name, std::string last_name);
+    static bool remove(long id);
+    void update();
+    void save();
+
+    Poco::JSON::Object::Ptr toJSON() const;
+};
+}  // namespace database
